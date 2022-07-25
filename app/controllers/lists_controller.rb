9f8b5f87f09950f97e3bf14f25fd9a288fe0c14d
EC2 Default User <ec2-user@ip-172-31-25-60.ap-northeast-1.compute.internal>
2022-07-25 12:26:11 +0000

    Add

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,39 @@
+class ListsController < ApplicationController
+  def new
+   #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する
+   @list = List.new
+  end
+  
+  #以下を追加
+  def create
+   #1.&2. データを受け取り新規登録するためのインスタンス作成
+   list = List.new(list_params)
+   #3. データをデータベースに保存するためのsaveメソッド実行
+   list.save
+   #4. トップ画面へリダイレクト
+   redirect_to list_path(list.id) 
+  end
+
+  def index
+   @lists = List.all
+  end
+
+  def show
+   @list = List.find(params[:id])
+  end
+
+  def edit
+   @list = List.find(params[:id])
+  end
+  
+  def update
+   list = List.find(params[:id])
+   list.update(list_params)
+   redirect_to list_path(list.id)
+  end
+  
+  private
+  def list_params
+   params.require(:list).permit(:title, :body, :image)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+  
+  get 'top' => 'home#top'
+  post 'lists' => 'lists#create' #ここを追記
+  get 'lists' => 'lists#index'
+ 
+ #.../lists/1や...lists/3に該当する
+ get 'lists/:id' => 'lists#show' , as: 'list'
+ get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+ patch 'lists/:id' => 'lists#update', as: 'update_list'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
