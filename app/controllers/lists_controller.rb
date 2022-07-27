31d50380e31ad49d69ee8d0421e9acfeb7640c40
EC2 Default User <ec2-user@ip-172-31-25-60.ap-northeast-1.compute.internal>
2022-07-27 14:24:04 +0000

    Add

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,11 +7,14 @@ class ListsController < ApplicationController
   #以下を追加
   def create
    #1.&2. データを受け取り新規登録するためのインスタンス作成
-   list = List.new(list_params)
+   @list = List.new(list_params)
    #3. データをデータベースに保存するためのsaveメソッド実行
-   list.save
+   if @list.save
    #4. トップ画面へリダイレクト
-   redirect_to list_path(list.id) 
+     redirect_to list_path(@list.id)
+   else
+    render :new
+   end
   end
 
   def index
@@ -32,9 +35,9 @@ class ListsController < ApplicationController
    redirect_to list_path(list.id)
   end
   
-  def destoroy
+  def destroy
     list = List.find(params[:id]) # データ（レコードを一件取得
-    list.destoroy　#データ（レコード）を削除
+    list.destroy #データ（レコード）を削除
     redirect_to '/lists' #投稿一覧画面の削除
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
+ 
   get 'top' => 'home#top'
-  post 'lists' => 'lists#create' #ここを追記
-  get 'lists' => 'lists#index'
- #.../lists/1や...lists/3に該当する
- get 'lists/:id' => 'lists#show' , as: 'list'
- get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
- patch 'lists/:id' => 'lists#update', as: 'update_list'
- delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
